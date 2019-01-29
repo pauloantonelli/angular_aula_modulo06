1b1282fa91ea8f70b9feed01d49981e2da1ea1e3
pauloantonelli <pauloantonelli@hotmail.com>
2019-01-29 18:49:46 -0200

    componentes e materialize iniciais criados

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+
+@NgModule({
+  declarations: [
+    CursosComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
