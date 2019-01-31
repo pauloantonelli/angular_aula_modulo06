16244c50d0471b4cedf22d975a1d3a5b8e7036e8
pauloantonelli <pauloantonelli@hotmail.com>
2019-01-31 19:07:02 -0200

    final da aula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
