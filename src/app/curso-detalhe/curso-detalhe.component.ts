35834ffbad123366d190419f95c4904ff7cda0b2
pauloantonelli <pauloantonelli@hotmail.com>
2019-02-05 14:25:16 -0200

    fim da aula

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.scss']
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
   public id: string;
-  constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
-  }
+  public inscricao: Subscription;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe((parametros: any) => {
+      this.id = parametros['id'];
+      console.log(this.id);
+    });
   }
-
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
 }
