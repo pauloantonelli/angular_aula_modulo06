055032a026f066bc6bdacd1aa44801eea8e3cc73
pauloantonelli <pauloantonelli@hotmail.com>
2019-02-06 14:28:45 -0200

    fim da aula

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CursosService } from './cursos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
   public cursos: any [] = [];
-  constructor(private _cursosService: CursosService) { }
+  public pagina: number;
+  public inscricao: Subscription;
+  constructor(private _cursosService: CursosService, private _routes: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
     this.cursos = this._cursosService.getCursos();
-  }
 
+    this.inscricao = this._routes.queryParams.subscribe((parametro: any) => {
+      console.log(parametro);
+      this.pagina = parametro['pagina'];
+    });
+  }
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+  proximaPagina() {
+    this.pagina ++;
+    this._router.navigate(['cursos'], {
+      queryParams: {'pagina': this.pagina}
+    });
+  }
 }
