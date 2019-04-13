ca56753cecf6f536c6d6f426699280629db04fd1
pauloantonelli <pauloantonelli@hotmail.com>
2019-04-13 16:14:51 -0300

    fim da aula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth-guard/auth.guard';
 import { CursosGuard } from './guards/auth-guard/cursos.guard';
 import { AlunosGuard } from './guards/auth-guard/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 const routes: Routes = [
   // lazy loading
@@ -14,8 +15,10 @@ const routes: Routes = [
   { path: 'alunos', loadChildren: './alunos/alunos.module#AlunosModule', canActivate: [AuthGuard] },
   // lazy loading
 
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PaginaNaoEncontradaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { HomeModule } from './home/home.module';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 // import { CursosModule } from './cursos/cursos.module'; removido por causa do lazy loading
 // import { AlunosModule } from './alunos/alunos.module'; removido por causa do lazy loading
 
 @NgModule({
   declarations: [
     AppComponent,
+    PaginaNaoEncontradaComponent,
   ],
   imports: [
     BrowserModule,
